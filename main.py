90615627518233be722f10ab631718c06d4da730
Ocelotsega <jordicromicon777@gmail.com>
2023-11-27 13:28:03 -0600

    Commit

--- main.py
+++ main.py
@@ -23,16 +23,25 @@ modelo_final=pd.read_parquet("DatosML/ModeloFinal.parquet")
 def read_root():
     return {"message": "Proyecto Individual"}
 
+# Funciones de validación y optimización
 
-
-
-# Primera función
 def validate_genre(genre):
+    """
+    Valida si el género existe en el conjunto de datos.
+    """
     genre_name = genre.capitalize()
     df_filtered = games[games['genres'].str.contains(genre, case=False, na=False)]
     if df_filtered.empty:
         raise HTTPException(status_code=404, detail=f"Género {genre_name} no encontrado")
 
+def validate_year(year):
+    """
+    Valida si el año existe en el conjunto de datos.
+    """
+    if year not in developer_opinion['release_date'].unique():
+        raise HTTPException(status_code=404, detail=f"Año {year} no encontrado")
+
+# Primera función optimizada
 @app.get('/UserForGenre')
 def user_for_genre(genre: str):
     """
@@ -55,17 +64,7 @@ def user_for_genre(genre: str):
     }
     return result
 
-
-
-#Segunda función
-
-
-# Validar año
-def validate_year(year):
-    if year not in developer_opinion['release_date'].unique():
-        raise HTTPException(status_code=404, detail=f"Año {year} no encontrado")
-
-# Obtener top 3 de juegos MÁS recomendados
+# Segunda función optimizada
 @app.get('/UsersRecommend', response_model=List[dict])
 def users_recommend(year: int):
     """
@@ -73,26 +72,14 @@ def users_recommend(year: int):
     """
     validate_year(year)
 
-    # Filtrar reseñas para el año y con recomendación positiva
     df_filtered = developer_opinion[(developer_opinion['release_date'] == year) & (developer_opinion['recommend'] == True)]
-
-    # Ordenar por puntuación positiva en orden descendente y tomar las primeras 3
     df_sorted = df_filtered.sort_values(by='Positivo', ascending=False).head(3)
 
-    # Crear el resultado con el formato especificado
     result = [{"Puesto {}".format(i+1): {"Título": title, "Puntuación Positiva": positive_score}} for i, (title, positive_score) in enumerate(zip(df_sorted['title'], df_sorted['Positivo']))]
     
     return result
 
-
-
-#Tercera Función
-# Validar año
-def validate_year(year):
-    if year not in developer_opinion['release_date'].unique():
-        raise HTTPException(status_code=404, detail=f"Año {year} no encontrado")
-
-# Obtener top 3 de desarrolladoras con juegos MENOS recomendados
+# Tercera función optimizada
 @app.get('/UsersWorstDeveloper', response_model=List[dict])
 def users_worst_developer(year: int):
     """
@@ -100,52 +87,47 @@ def users_worst_developer(year: int):
     """
     validate_year(year)
 
-    # Filtrar reseñas para el año y con recomendación negativa
     df_filtered = developer_opinion[(developer_opinion['release_date'] == year) & (developer_opinion['sentiment_analisis'].isin([0, 1, 2]))]
-
-    # Agrupar por desarrolladora y contar las reseñas por categoría
     df_grouped = df_filtered.groupby(['developer', 'sentiment_analisis'])['sentiment_analisis'].count().unstack(fill_value=0).reset_index()
-
-    # Renombrar las columnas
     df_grouped.columns = ['developer', 'Neutral', 'Negativo', 'Positivo']
-
-    # Ordenar por puntos negativos en orden descendente y tomar las primeras 3
     df_sorted = df_grouped.sort_values(by='Negativo', ascending=False).head(3)
 
-    # Crear el resultado con el formato especificado
     result = [{"Puesto {}".format(i+1): {"Desarrolladora": developer, "Puntuación Negativa": negative_points}} for i, (developer, negative_points) in enumerate(zip(df_sorted['developer'], df_sorted['Negativo']))]
     
     return result
 
-
-
-
-#cuarta función 
+# Cuarta función optimizada
 @app.get('/sentiment_analysis')
 def sentiment_analysis(developer: str):
     """
     Devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros
     de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
     """
-    # Convertir la columna 'developer' a str y filtrar valores nulos o extraños
     df_filtered = developer_opinion[developer_opinion['developer'].notnull()]
     df_filtered['developer'] = df_filtered['developer'].astype(str)
-
-    # Filtrar por desarrolladora
     df_filtered = df_filtered[df_filtered['developer'] == developer]
 
     if df_filtered.empty:
         raise HTTPException(status_code=404, detail=f"Desarrolladora '{developer}' no encontrada")
 
-    # Realizar el análisis de sentimiento
     sentiment_counts = df_filtered['sentiment_analisis'].value_counts().to_dict()
-
-    # Ajustar el resultado para reflejar que 2 es positivo, 0 es neutral y 1 es negativo
     adjusted_sentiments = {2: "Positivo", 0: "Neutral", 1: "Negativo"}
     sentiment_counts = {adjusted_sentiments[key]: value for key, value in sentiment_counts.items()}
 
     result = {developer: sentiment_counts}
     return result
 
+# Modelo de recomendación optimizado
+@app.get('/encontrar-juegos-similares/{id_juego}', response_model=List[str])
+def encontrar_juegos_similares_endpoint(id_juego: int):
+    try:
+        juego_indice = modelo_final.index[modelo_final['id'] == id_juego].item()
+    except ValueError:
+        raise HTTPException(status_code=404, detail=f"El juego con el ID {id_juego} no existe en la base de datos.")
 
+    juego_caracteristicas = modelo_final.iloc[juego_indice, 3:].values.reshape(1, -1)
+    similitudes_render = cosine_similarity(modelo_final.iloc[:, 3:], juego_caracteristicas)
+    indices_juegos_similares = similitudes_render.argsort(axis=0)[::-1][1:6].flatten()[1:]
+    juegos_similares = modelo_final.iloc[indices_juegos_similares, 'title'].tolist()
 
+    return juegos_similares
